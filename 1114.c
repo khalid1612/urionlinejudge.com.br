38e77e73060a5fca7d3ff35b500d382e4c39a5e0
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 20:39:44 +0600

    Create 1114.c

---
+++ 1114.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    while(1)
+    {
+        int a;
+        scanf("%d",&a);
+
+        if(a == 2002){
+            printf("Acesso Permitido\n");
+            break;
+        }
+        else{
+            printf("Senha Invalida\n");
+        }
+    }
+
+    return 0;
+}
+
