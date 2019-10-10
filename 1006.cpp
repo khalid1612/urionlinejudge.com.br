cda00edf90a281ba83267fc78115f06131b3373e
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 20:05:36 +0600

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    float A,B,C;
+    scanf("%f %f %f",&A,&B,&C);
+    printf("MEDIA = %.1f\n",(A*2 + B*3 + C*5)/10);
+
+ 
+    return 0;
+}
