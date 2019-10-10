c197a804f8b448d75d8bf8e1d70f710e0ae4e421
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 21:11:23 +0600

    Create 1184.c

---
+++ 1184.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    scanf("%c",&c);
+
+    float sum = 0.0,num;
+    int i,j;
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 12; j++) {
+            scanf("%f",&num);
+            if(j < i)
+                sum += num;
+        }
+    }
+
+    if(c == 'S')
+        printf("%.1f\n",sum);
+    else
+        printf("%.1f\n",sum/66.0);
+
+    return 0;
+}
