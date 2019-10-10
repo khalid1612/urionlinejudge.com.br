208e2e2da8f68fe3214c93e449484af1406f0066
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 21:10:02 +0600

    Create 1182.c

---
+++ 1182.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int line;
+    char c;
+    scanf("%d %c",&line,&c);
+
+    float sum = 0.0,num;
+    int i,j;
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 12; j++) {
+            scanf("%f",&num);
+            if(j == line)
+                sum += num;
+        }
+    }
+
+    if(c == 'S')
+        printf("%.1f\n",sum);
+    else
+        printf("%.1f\n",sum/12.0);
+
+    return 0;
+}
+
