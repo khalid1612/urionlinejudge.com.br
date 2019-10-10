1c76785ddde401a9992585069c5676e9c0cdf5b2
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 21:08:24 +0600

    Create 1175.c

---
+++ 1175.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[20];
+
+    int i;
+    for(i = 0; i < 20; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i = 0; i < 20; i++){
+        printf("N[%d] = %d\n",i,a[19-i]);
+    }
+
+    return 0;
+}
