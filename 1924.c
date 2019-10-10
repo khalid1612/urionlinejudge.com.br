ea05d9ef2907be92b2dd5dbad9991fdbc2d03da4
MD. KhaliD HassaN <45466581+khalid1612@users.noreply.github.com>
2019-10-10 21:17:08 +0600

    Create 1924.c

---
+++ 1924.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    getchar();
+    while(n--)
+    {
+        char s[110];
+        gets(s);
+    }
+    printf("Ciencia da Computacao\n");
+
+
+    return 0;
+}
